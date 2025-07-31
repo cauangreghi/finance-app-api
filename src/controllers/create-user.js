bb48bf0983f38ea7d8abb9ff087440262486ec73
Cauan Gregui <cauan.vivan@gubolincopartners.com.br>
2025-07-30 23:31:15 -0300

    feat: add validations on create-user-controller

---
+++ src/controllers/create-user.js
@@ -0,0 +1,46 @@
+import { CreateUserUseCase } from "../use-cases/create-user.js"
+import { badRequest, created, serverError } from "./helpers.js"
+import validator from "validator"
+
+export class CreateUserController {
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+
+            const requiredFields = [
+                "first_name",
+                "last_name",
+                "email",
+                "password",
+            ]
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length == 0) {
+                    return badRequest({ message: `Missing param: ${field}` })
+                }
+            }
+
+            if (params.password.length < 6) {
+                return badRequest({
+                    message: "Password must be at least 6 characters",
+                })
+            }
+
+            const emailIsValid = validator.isEmail(params.email)
+
+            if (!emailIsValid) {
+                return badRequest({
+                    message: "Invalid e-mail. Please provide a valid one",
+                })
+            }
+
+            const createUserUseCase = new CreateUserUseCase()
+            const createUser = await createUserUseCase.execute(params)
+
+            return created(createUser)
+        } catch (error) {
+            console.log(error)
+            return serverError()
+        }
+    }
+}
